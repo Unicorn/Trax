ad48ae6b2e7e431480fe64a594681bae96e0a69b
Clay McIlrath <clay.mcilrath@gmail.com>
2018-12-02 15:00:26 -0500

    upgrading packages and making auth compatible with webRequest

--- renderer/sagas/authSaga.ts
+++ renderer/sagas/authSaga.ts
@@ -17,12 +17,13 @@ declare global {
 }
 
 const createAuthWindow = () => {
+
   // Build the OAuth consent page URL
   const win = new window.BrowserWindow({
     width: 600,
     height: 750,
     show: true,
-    titleBarStyle: 'hidden-inset',
+    titleBarStyle: 'hiddenInset',
     webPreferences: {
       nodeIntegration: false,
     },
@@ -48,33 +49,12 @@ const getGithubAuthCode = () => {
   const contents = window.authWindow.webContents
 
   return eventChannel(emit => {
-    contents.on('did-fail-load', (_e: Event, _errCode: number, error: string, url: string) => {
-      if (!url.match(GITHUB.HOST)) return
-
-      console.log(`Invalid Hostname - ${_errCode}`, `Could not load ${GITHUB.HOST}. ${error}`)
-      emit({ error })
-    })
-
-    contents.on('will-navigate', (_e: Event, url: string) => {
-      if (!url.match(MICROSERVICE.API)) return
-
-      console.log('will-navigate', url)
-      contents.stop()
-      const { code, error } = parseGithubAuth(url)
-
+    const filter = { urls: [ `${MICROSERVICE.API}/*`] }
+    contents.session.webRequest.onBeforeRequest(filter, (details, callback) => {
+      const { code, error } = parseGithubAuth(details.url)
       emit({ code, error })
       emit(END)
-    })
-
-    contents.on('did-get-redirect-request', (_e: Event, _old: string, url: string) => {
-      if (!url.match(MICROSERVICE.API)) return
-
-      console.log('did-get-redirect-request', url)
-      contents.stop()
-      const { code, error } = parseGithubAuth(url)
-
-      emit({ code, error })
-      emit(END)
+      callback({})
     })
 
     return () => window.authWindow.destroy()
@@ -100,7 +80,6 @@ function* watchLogout() {
 
 function* watchAuthRequest() {
   yield createAuthWindow()
-
   const authCode = yield call(getGithubAuthCode)
   const { code, error } = yield take(authCode)
 
