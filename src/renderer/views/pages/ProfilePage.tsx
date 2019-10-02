d85fb02472ea4ab8c610f341386dd49c75f7b874
Clay Unicorn <thinkclay@gmail.com>
2019-10-02 18:44:34 -0400

    Small refactor to ProfilePage

--- src/renderer/views/pages/ProfilePage.tsx
+++ src/renderer/views/pages/ProfilePage.tsx
@@ -1,5 +1,5 @@
 /** @jsx createElement **/
-import { createElement, Component, ReactNode } from 'react'
+import { createElement, FC, ReactNode, useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import Tabbed from '@/views/ui/Tabbed'
@@ -29,31 +29,26 @@ interface State {
   logins: { [key: string]: string }
 }
 
-class ProfilePage extends Component<Connected & Actions, State> {
-  state = {
-    content: {},
-    logins: {}
-  }
+const ProfilePage: FC<Connected & Actions> = ({ tracks, repos, orgs, _getOrgs, _getReposForLogin }) => {
+  const [_state, _setState] = useState<State>({ content: {}, logins: {} })
 
-  componentWillMount(): void {
-    this.props._getOrgs()
-  }
+  useEffect(() => {
+    _getOrgs()
+  }, [])
 
-  componentWillReceiveProps(props: Connected): void {
-    const newState: State = { ...this.state }
-    const orgArr = toArray(props.orgs) as Org[]
+  useEffect(() => {
+    const newState: State = { ..._state }
+    const orgArr = toArray(orgs) as Org[]
 
     orgArr.forEach((org: Org) => {
       newState.content[org.login] = <RepoList repoIds={org.repoIds} />
       newState.logins[org.login] = org.nodeId
     })
 
-    this.setState(newState)
-  }
+    _setState(newState)
+  }, [orgs])
 
-  _renderTracks = (): ReactNode => {
-    const { tracks, repos }: Connected = this.props
-
+  const _renderTracks = (): ReactNode => {
     if (tracks.keys.length < 1) return <p>Nothing tracked yet. Select a repo to track.</p>
 
     const activeTracks = (toArray(tracks) as Track[]).filter(track => track.active)
@@ -67,31 +62,29 @@ class ProfilePage extends Component<Connected & Actions, State> {
     )
   }
 
-  _tabHandler = (login: string): void => {
-    const { logins } = this.state as State
+  const _tabHandler = (login: string): void => {
+    const { logins } = _state as State
     const key: string = logins[login]
-    this.props._getReposForLogin(login, key)
+    _getReposForLogin(login, key)
   }
 
-  render(): ReactNode {
-    return (
-      <section className="profile page">
-        <ProfileHelp />
+  return (
+    <section className="profile page">
+      <ProfileHelp />
 
-        <div className="columns golden-ratio">
-          <div className="left column">
-            <div className="scroll">
-              <h2>Tracked Repositories</h2>
-              {this._renderTracks()}
-            </div>
-          </div>
-          <div className="right column">
-            <Tabbed content={this.state.content} tabHandler={this._tabHandler} />
+      <div className="columns golden-ratio">
+        <div className="left column">
+          <div className="scroll">
+            <h2>Tracked Repositories</h2>
+            {_renderTracks()}
           </div>
         </div>
-      </section>
-    )
-  }
+        <div className="right column">
+          <Tabbed content={_state.content} tabHandler={_tabHandler} />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 const mapState = (state: RootState): Connected => ({
